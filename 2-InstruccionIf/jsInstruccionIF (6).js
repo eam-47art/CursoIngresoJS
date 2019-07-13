2d552cf5bd1d163830a0e2c2dcc54ac0e1f80430
alumno <alumno>
2019-07-13 12:46:03 -0300

    clase del sabado

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,23 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+
+edad = document.getElementById("edad").value;
+
+if (edad<13)
+
+{ alert("Es un niño");}
+
+else
+if (edad>=18)
+
+{ alert("Es mayor de edad");}
+
+else
+if ((edad>=13) && (edad<=17))
+
+{ alert("Es adolesente");}
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,6 +1,24 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+var estado;
+
+edad = document.getElementById("edad").value;
+estado= document.getElementById("estado").value;
+
+
+if (edad<18)
+{
+if(estado == soltero)
+} 
+{
+ alert("Es muy pequeño para no ser soltero");
+}
+
+
+
+
 
 	
 
